cc1f5af4588ea67b82730d23e40adfed1e5d9ae0
afirmae <nilashis37@gmail.com>
2024-04-04 11:18:37 +0530

    Refactor string trimming logic

--- Useless/Trim_string.java
+++ Useless/Trim_string.java
@@ -2,14 +2,19 @@ public class Trim_string
 {
     public static void main(String[] args) 
     {
-        String s = "  Hello   World  "; 
+        boolean first_ltr = false;
+        String s = "     Hello      World      and   Good     Morning      "; 
         String k = "";
 
         for (int i = 0; i < s.length(); i++) 
         {
             if (s.charAt(i) != ' ' || (i + 1 < s.length() && s.charAt(i + 1) != ' ')) 
             {
+                if (first_ltr || s.charAt(i) != ' ') 
+                {
                     k += s.charAt(i);
+                    first_ltr = true;
+                }
             }
         }
         System.out.println("Trimmed String: " + k + " of Length: " + k.length());
