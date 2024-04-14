f559b91657ac1816d8ec8460c908b49b3abaf945
afirmae <nilashis37@gmail.com>
2024-04-14 16:30:00 +0530

    Add ActivitySelection class with printMaxActivities method

---
+++ ActivitySelection.java
@@ -0,0 +1,27 @@
+class ActivitySelection 
+{
+    public static void printMaxActivities(int s[], int f[], int n) {
+        int i = 0;
+
+        System.out.println("Following activities are selected");
+        System.out.print("A" + (1 + i) + " ");
+
+        for (int j = 1; j < s.length; j++) 
+        {
+            if (s[j] >= f[i]) 
+            {
+                System.out.print("A" + (1 + j) + " ");
+                i = j;
+            }
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) 
+    {
+        int start[] = { 1, 3, 2, 4, 8, 7, 9, 11, 9, 12 };
+        int finish[] = { 3, 4, 5, 7, 9, 10, 11, 12, 13, 14 };
+
+        printMaxActivities(start, finish, start.length);
+    }
+}
