c71e05966c475645567aeeda0e8fc488acd7ec87
afirmae <nilashis37@gmail.com>
2024-04-14 16:30:36 +0530

    Refactor ActivitySelection class to remove unnecessary parameter in printMaxActivities method

--- ActivitySelection.java
+++ ActivitySelection.java
@@ -1,6 +1,6 @@
 class ActivitySelection 
 {
-    public static void printMaxActivities(int s[], int f[], int n) {
+    public static void printMaxActivities(int s[], int f[]) {
         int i = 0;
 
         System.out.println("Following activities are selected");
@@ -22,6 +22,6 @@ class ActivitySelection
         int start[] = { 1, 3, 2, 4, 8, 7, 9, 11, 9, 12 };
         int finish[] = { 3, 4, 5, 7, 9, 10, 11, 12, 13, 14 };
 
-        printMaxActivities(start, finish, start.length);
+        printMaxActivities(start, finish);
     }
 }
