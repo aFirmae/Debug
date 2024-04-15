363b01218635e85dc940f18aa731dba5ff852e11
afirmae <nilashis37@gmail.com>
2024-04-15 21:28:14 +0530

    Add String_Exch.java for exchanging characters in a string

--- Useless/Remove_lead_zero.java
+++ Useless/Remove_lead_zero.java
@@ -4,22 +4,21 @@ public class Remove_lead_zero
     public static void main(String[] args)
     {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the number");
+        
+        System.out.print("Enter the number: ");
         String num = sc.nextLine();
+
         System.out.println("Number without leading zeroes: " + removeLeadingZeros(num));
     }
     public static String removeLeadingZeros(String num)
     {
         String result = "";
-        boolean  flag = false;
-        for (int i = 0; i < num.length(); i++)
-        {
-            if (num.charAt(i) != '0' || flag)
-            {
-                result += num.charAt(i);
-                if (num.charAt(i) != '0') flag = true;
-            }
-        }
+        int i = 0;
+
+        while (i < num.length() && num.charAt(i) == '0') i++;
+
+        while (i < num.length()) result += num.charAt(i++);
+
         return result;
     }
 }
