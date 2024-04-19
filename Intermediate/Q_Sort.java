6dbebf29194db54b43a12cadcf68c0f0cf658555
afirmae <nilashis37@gmail.com>
2024-04-19 20:04:15 +0530

    Refactor Q_Sort.java to fix pivot selection and improve sorting algorithm

--- Intermediate/Q_Sort.java
+++ Intermediate/Q_Sort.java
@@ -4,21 +4,22 @@ public class Q_Sort
 {
     public static void main(String[] args)
     {
-        int[] arr = {69, 2, 25, 100, 50, 10};
+        int[] arr = {180, 165, 170};
         Q_Sort obj = new Q_Sort();
         obj.quickSort(arr, 0, arr.length - 1);
         System.out.println("Sorted Array: " + Arrays.toString(arr));
     }
+    
     void quickSort(int[] arr, int low, int high)
     {
-        int pi;
         if(low < high)
         {
-            pi = split(arr, low, high);
+            int pi = split(arr, low, high);
             quickSort(arr, low, pi - 1);
             quickSort(arr, pi + 1, high);
         }
     }
+
     int split(int[] arr, int lb, int ub)
     {
         int pivot = arr[lb];
@@ -26,13 +27,14 @@ public class Q_Sort
 
         while(p < q)
         {
-            while(p <= q && arr[p] <= pivot) p++;
-            while(p <= q && arr[q] > pivot) q--;
+            while(p <= q && arr[p] >= pivot) p++;
+            while(p <= q && arr[q] < pivot) q--;
             if(p < q) swap(arr, p, q);
         }
         swap(arr, lb, q);
         return q;
     }
+
     void swap(int[] arr, int i, int j)
     {
         int temp = arr[i];
