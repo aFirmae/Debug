be70e4fd5f073fc9b130ae2b3fcdc861c48ce34e
afirmae <nilashis37@gmail.com>
2024-06-25 00:01:12 +0530

    Commit

--- Practice/Own_Exception.java
+++ Practice/Own_Exception.java
@@ -6,7 +6,7 @@ class Own_Exception extends Exception {
         int a = 10, b = 3;
         try {
             float quo = (float) a / b;
-            if (quo < 1) throw new MyException("Number is very small");
+            if (quo < 1) throw new Own_Exception("Number is very small");
         } 
         catch (MyException e) {
             System.out.println("Caught");
